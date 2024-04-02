a0099de21330a0a7c4e0644654b0c65fe05ca4cb
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-04-02 10:40:57 +0200

    both vhdl and verilog files are now used for synthesis

--- eda_tools/vivado/tcl/analyze_script.tcl
+++ eda_tools/vivado/tcl/analyze_script.tcl
@@ -85,13 +85,32 @@ if {[catch {
     ######################################
     #set filenames [split [exec find $rtl_path/ -type f ( -name *.sv -o -name *.v )] \n]
     #set filenames [split [exec find $rtl_path/core $rtl_path/soc -type f -name *.sv ! -name soc_config.sv ] \n]
-    set filenames [split [exec find $tmp_path/rtl/ -type f -name *$rtl_file_format] \n]
 
-    #read_verilog $filenames
-    if {[catch {read_verilog $filenames} errmsg]} {
-        puts "<green>analyze_script.tcl<end>: <bold><red>error: failed reading source files, exiting<end>"
-        puts "<green>analyze_script.tcl<end>: tool says -> $errmsg"
-        exit -1
+    # read verilog source files
+    set verilog_filenames [split [exec find $tmp_path/rtl/ -type f ( -name *.v -o -name *.sv )] \n]
+    if {[catch {read_verilog $verilog_filenames} errmsg]} {
+        if {$verilog_filenames == ""} {
+            puts "<green>analyze_script.tcl<end>: <cyan>note: no verilog file in source directory<end>"
+        } {
+            puts "<green>analyze_script.tcl<end>: <bold><red>error: failed reading verilog source files<end>"
+            puts "<green>analyze_script.tcl<end>: tool says -> $errmsg"
+        }
+        set verilog_error 1
+    }
+
+    # read vhdl source files
+    set vhdl_filenames [split [exec find $tmp_path/rtl/ -type f ( -name *.vhd -o -name *.vhdl )] \n]
+    if {[catch {read_vhdl $vhdl_filenames} errmsg]} {
+        if {$vhdl_filenames == ""} {
+            puts "<green>analyze_script.tcl<end>: <cyan>note: no vhdl file in source directory<end>"
+        } else {
+            puts "<green>analyze_script.tcl<end>: <bold><red>error: failed reading vhdl source files<end>"
+            puts "<green>analyze_script.tcl<end>: tool says -> $errmsg"
+        }
+        if {$verilog_error == 1} {
+            puts "<green>analyze_script.tcl<end>:<red>error: failed reading both verilog and vhdl source files, exiting"
+            exit -1
+        }
     }
 
 } ]} {
