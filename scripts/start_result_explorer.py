955adbc072c4728834026bba99887f4d341ce428
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-04-09 11:30:13 +0200

    improved result explorer start/stop

--- scripts/start_result_explorer.py
+++ scripts/start_result_explorer.py
@@ -23,7 +23,10 @@ import os
 import sys
 import webbrowser
 from threading import Thread
-from result_explorer import app
+import tty
+import termios
+import result_explorer
+import select
 
 host_address = '127.0.0.1'
 port = 8052
@@ -32,14 +35,41 @@ def open_browser():
     webbrowser.open(host_address + ':' + str(port), new=0, autoraise=True)
 
 def close_server():
-    sys.exit()
+    restore_mode(old_settings)
+    print('\r')
+    os._exit(0)
+
+def set_raw_mode():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setraw(fd)
+    return old_settings
+
+def restore_mode(old_settings):
+    fd = sys.stdin.fileno()
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 if __name__ == "__main__":
     from waitress import serve
     from flask import request
 
+    print("result explorer server running on http://" + host_address + ":" + str(port) + '/')
+    print("press 'q' to quit")
+
     # Open the web page
     process = Thread(target=open_browser).start()
 
     # Start the server
-    serve(app.server, host=host_address, port=port)
+    serve_thread = Thread(target=serve, args=(result_explorer.app.server,), kwargs={'host': host_address, 'port': port})
+    serve_thread.start()
+
+    old_settings = set_raw_mode()
+    try:
+        while True:
+            # Check if a key is pressed
+            if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+                key = sys.stdin.read(1).lower()
+                if key == 'q':
+                    close_server()
+    finally:
+        restore_mode(old_settings)
