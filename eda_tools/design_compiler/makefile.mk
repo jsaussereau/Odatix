4b78d68cbaf5bd2767cd5cc5759f5e48239a7a9e
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-04-15 18:06:08 +0200

    added a makefile for design_compiler

--- eda_tools/design_compiler/makefile.mk
+++ eda_tools/design_compiler/makefile.mk
@@ -35,14 +35,12 @@ INIT_SCRIPT             = init_script.tcl
 ANALYZE_SCRIPT          = analyze_script.tcl
 SYNTH_SCRIPT            = synth_script.tcl
 SYNTH_FREQ_SCRIPT       = find_fmax.tcl
-EXIT_SCRIPT             = exit.tcl
 
 ########################################################
 # Tool specific
 ########################################################
 
-VIVADO					= vivado
-VIVADO_INIT             = export LC_ALL=C; unset LANGUAGE;
+DC_COMPILER				= design_vision
 
 ########################################################
 # Text formatting
@@ -59,94 +57,20 @@ _MAGENTA                =\x1b[35m
 _CYAN                   =\x1b[36m
 _WHITE                  =\x1b[37m
 
-HASH := \#
-
-VIVADO_COLOR            = "s/INFO/$(_CYAN)INFO$(_END)/;s/WARNING/$(_YELLOW)WARNING$(_END)/;s/ERROR/$(_RED)$(_BOLD)ERROR$(_END)/;s/<green>/$(_GREEN)/;s/<red>/$(_RED)/;s/<yellow>/$(_YELLOW)/;s/<cyan>/$(_CYAN)/;s/<blue>/$(_BLUE)/;s/<magenta>/$(_MAGENTA)/;s/<bold>/$(_BOLD)/;s/<end>/$(_END)/g"
+DC_COLOR				= "s/Information/$(_CYAN)Information$(_END)/;s/Warning/$(_YELLOW)Warning$(_END)/;s/Error/$(_RED)$(_BOLD)Error$(_END)/;s/<green>/$(_GREEN)/;s/<red>/$(_RED)/;s/<cyan>/$(_CYAN)/;s/<bold>/$(_BOLD)/;s/<end>/$(_END)/;s/<Err0r>/Error/;s/<W4rning>/Warning/"
 
 ########################################################
 # Rules
 ########################################################
 
-.PHONY: all
-all: motd synth par
-
-
-.PHONY: clean
-clean:
-	@rm -f ./*.jou
-	@rm -f ./vivado*.log
-
-.PHONY: motd
-motd:
-	@echo -ne "$(_BOLD)"
-	@echo "********************************************************************"
-	@echo "*                           Vivado Flow                            *"
-	@echo "********************************************************************"
-	@echo -ne "$(_END)"
-	@echo ""
-
-.PHONY: help
-help:
-	@echo -e "SYNTHESIS"
-	@echo -e "\t$(_BOLD)make analyze$(_END): run $(ANALYZE_SCRIPT) (check syntax)"
-	@echo -e "\t$(_BOLD)make synth$(_END): run the whole synthesis script"
-	@echo -e "\t$(_BOLD)make synth_fmax$(_END): run synthesis with a binary search to find max frequency"
-	@echo -e "OTHERS"
-	@echo -e "\t$(_BOLD)make help$(_END): display a list of useful rules"
-
-########################################################
-# Synthesis
-########################################################
-
-.PHONY: analyze
-analyze: motd analyze_only clean
-
-.PHONY: analyze_only
-analyze_only: logdir
-	@$(VIVADO_INIT)\
-	$(VIVADO) -mode tcl -notrace \
-	-source $(SCRIPT_DIR)/$(INIT_SCRIPT) \
-	-source $(SCRIPT_DIR)/$(ANALYZE_SCRIPT) \
-	| tee $(LOG_DIR)/$(ANALYZE_SCRIPT).log | sed $(VIVADO_COLOR)
-	@echo "result logged to \"$(LOG_DIR)/$(ANALYZE_SCRIPT).log\""
-
-.PHONY: synth_fmax
-synth_fmax: motd synth_fmax_only clean
-
 .PHONY: synth_fmax_only
 synth_fmax_only: logdir
-	@$(VIVADO_INIT)\
-	$(VIVADO) -mode tcl -notrace \
-	-source $(SCRIPT_DIR)/$(SYNTH_FREQ_SCRIPT) \
-	-source $(SCRIPT_DIR)/$(EXIT_SCRIPT) \
+	$(DC_COMPILER) -x "gui_stop; source $(SCRIPT_DIR)/$(SYNTH_FREQ_SCRIPT); quit" \
 	| tee $(LOG_DIR)/$(SYNTH_FREQ_SCRIPT).log \
-	| sed $(VIVADO_COLOR); \
+	| sed $(DC_COLOR); \
 	EXIT_CODE=$${PIPESTATUS[0]}; \
 	echo "result logged to \"$(LOG_DIR)/$(SYNTH_FREQ_SCRIPT).log\""; \
 	exit $$EXIT_CODE
-
-.PHONY: test
-test:
-	@exit 0
-
-.PHONY: synth
-synth: motd synth_only clean
-
-.PHONY: synth_only
-synth_only: logdir
-	@$(VIVADO_INIT)\
-	$(VIVADO) -mode tcl -notrace \
-	-source $(SCRIPT_DIR)/$(INIT_SCRIPT) \
-	-source $(SCRIPT_DIR)/$(ANALYZE_SCRIPT) \
-	-source $(SCRIPT_DIR)/$(SYNTH_SCRIPT) \
-	-source $(SCRIPT_DIR)/$(EXIT_SCRIPT) \
-	| tee $(LOG_DIR)/$(SYNTH_SCRIPT).log | sed $(VIVADO_COLOR)
-
-.PHONY: vivado
-vivado:
-	@$(VIVADO_INIT)\
-	$(VIVADO) -mode tcl -notrace \
-	| sed $(VIVADO_COLOR)
 
 .PHONY: logdir
 logdir:
