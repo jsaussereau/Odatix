d07943ee4ae528d2a20f1f622f89ca5c4cf8482c
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-04-26 11:55:15 +0200

    Change design kit path

--- eda_tools/design_compiler/technology/XFAB_180nm_CMOS/synopsys_dc_setup.tcl
+++ eda_tools/design_compiler/technology/XFAB_180nm_CMOS/synopsys_dc_setup.tcl
@@ -2,36 +2,33 @@
 ##     Initialization Setup file for Synopsys Design Compiler      ##
 #####################################################################
 
-set X_DIR [get_unix_variable X_DIR]
-set SYNOPSYS [get_unix_variable SYNOPSYS]
+define_design_lib WORK -path "./work"
 
-#set search_path	". $X_DIR/xh018/diglibs/IO_CELLS_3V/v2_1/cadence_IC61/v2_1_2/IO_CELLS_3V $X_DIR/xh018/diglibs/IO_CELLS_F3V/v2_1/cadence_IC61/v2_1_2/IO_CELLS_F3V $X_DIR/xh018/diglibs/D_CELLS_HD/v3_0/cadence_IC61/v3_0_1/D_CELLS_HD $SYNOPSYS/libraries/syn $SYNOPSYS/dw/sim_ver"
+### retrieve Xfab directory path and SYNOPSYS path from the environement variables
+set XKIT ~/xfab180n/XKIT
+set XFABDIR $XKIT/xh018
+set SYNDIR /imssofts/softs/Synopsys/DesignCompiler/2013.03-SP5
 
-#set search_path	"."
-
-
-#set target_library "*.db"
-
-#set symbol_library "D_CELLS_HD.sdb"
+### set library paths
+set search_path	". $XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/trunk/pdk/xh018/diglibs/D_CELLS_HD/v3_0/liberty_LPMOS/v3_0_0/PVT_1_80V_range $SYNDIR/libraries/syn $SYNDIR/dw/sim_ver ../rtl ./db ./"
+##set symbol_library "D_CELLS_HD_LPMOS_typ_1_80V_25C.sdb" => cannot find sdb file, so this line is commented so that the generic symbols from synopsys installation are used
+set target_library "D_CELLS_HD_LPMOS_typ_1_80V_25C.db"
 
 # set synthetic_library dw_foundation.sldb 
 set link_library "* $target_library $synthetic_library"
 
-define_design_lib WORK -path ./work
-
+### set bus naming style for compliance between .sdf and post syn .vhdl
 set sdfout_no_edge  true
-set verilogout_equation	false
-set verilogout_no_tri	true 
-set verilogout_single_bit  false
-set verilogout_show_unconnected_pins true
+#set verilogout_equation	false
+#set verilogout_no_tri	true 
+#set verilogout_single_bit  false
+
 set hdlout_internal_busses true     
-set bus_inference_style "%s\[%d\]"  
-set bus_naming_style    "%s\[%d\]"
+set vhdlout_single_bit user
+set vhdlout_preserve_hierarchical_types user
+set bus_inference_style "%s_%d_"  
+set bus_naming_style    "%s_%d"
 set write_name_nets_same_as_ports true
 
-puts "USE: set_fix_multiple_port_nets -all [all_designs]"
-puts "change_names -rules verilog -hierarchy -verbose > change_names.v"
-puts "change_names -rules vhdl -hierarchy -verbose > change_names.vhd"
-
-
-
+#puts "USE: set_fix_multiple_port_nets -all [all_designs]"
+define_design_lib WORK -path "./work"
