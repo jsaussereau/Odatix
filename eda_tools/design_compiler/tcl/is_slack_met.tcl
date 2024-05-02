13f412d64054b27e6fd192dd92ee2e8de7a497a8
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-05-02 18:55:55 +0200

    Improve unconstrained path detection

--- eda_tools/design_compiler/tcl/is_slack_met.tcl
+++ eda_tools/design_compiler/tcl/is_slack_met.tcl
@@ -34,9 +34,13 @@ proc is_slack_met {timing_rep} {
 
 proc is_slack_inf {timing_rep} {
   set tfile [open $timing_rep]
-  #check if we can find "Slack:                    inf" in the timing report
+  #check if we can find "Path is unconstrained" or "No paths." in the timing report
   while {[gets $tfile data] != -1} {
-    if {[string match *[string toupper "(Path is unconstrained)"]* [string toupper $data]]} {
+    if {[string match *[string toupper "Path is unconstrained"]* [string toupper $data]]} {
+      close $tfile
+      return 1  
+    }
+    if {[string match *[string toupper "No paths."]* [string toupper $data]]} {
       close $tfile
       return 1  
     }
