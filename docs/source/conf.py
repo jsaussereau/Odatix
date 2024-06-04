e67ecf8742106a42b833bc08acc5c4c2a73098d0
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-06-04 11:10:01 +0200

    Change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'Asterism'
 copyright = '2022, Jonathan Saussereau'
 author = 'Jonathan Saussereau'
 
-release = '0.1'
-version = '0.1.0'
+#release = '0.1'
+#version = '0.1.0'
 
 # -- General configuration
 
@@ -32,8 +32,9 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 #html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 #html_theme = 'sphinx_book_theme'
-html_theme = 'sphinxawesome_theme'
+#html_theme = 'sphinxawesome_theme'
 
 html_static_path = ['_static']
 html_css_files = [
