c15567a6f4cef1026188df5cf94fdcf303e1bf92
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-07-11 10:55:10 +0200

    Add a main

--- scripts/start_result_explorer.py
+++ scripts/start_result_explorer.py
@@ -38,73 +38,102 @@ sys.path.append(lib_path)
 
 import printc
 
-script_name = "result_explorer.py"
+######################################
+# Parse Arguments
+######################################
 
-# Argument parser setup
-parser = argparse.ArgumentParser(description='Asterism - Start Result Explorer')
-parser.add_argument('-i', '--input', type=str, default='results', help='Directory of the result YAML files')
-parser.add_argument('-n', '--network', action='store_true', help='Run the server on the network')
-args = parser.parse_args()
+def add_arguments(parser):
+  parser.add_argument('-i', '--input', type=str, default='results', help='Directory of the result YAML files')
+  parser.add_argument('-n', '--network', action='store_true', help='Run the server on the network')
 
-# Default ip address: local
-host_address = '127.0.0.1'
-ip_address = host_address
-port = 8052
-network = args.network
+def parse_arguments():
+  parser = argparse.ArgumentParser(description='Asterism - Start Result Explorer')
+  add_arguments(parser)
+  return parser.parse_args()
+
+######################################
+# Misc functions
+######################################
 
 def open_browser():
-    webbrowser.open("http://" + ip_address + ':' + str(port), new=0, autoraise=True)
+  webbrowser.open("http://" + ip_address + ':' + str(port), new=0, autoraise=True)
 
 def close_server():
-    restore_mode(old_settings)
-    print('\r')
-    os._exit(0)
+  restore_mode(old_settings)
+  print('\r')
+  os._exit(0)
 
 def set_raw_mode():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    tty.setraw(fd)
-    return old_settings
+  fd = sys.stdin.fileno()
+  global old_settings
+  old_settings = termios.tcgetattr(fd)
+  tty.setraw(fd)
+  return old_settings
 
 def restore_mode(old_settings):
-    fd = sys.stdin.fileno()
-    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+  fd = sys.stdin.fileno()
+  termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+def start_result_explorer(input, network=False):
+
+  global ip_address
+  global port
+
+  script_name = "result_explorer.py"
+
+  # Default ip address: local
+  host_address = '127.0.0.1'
+  ip_address = host_address
+  port = 8052
+
+  from waitress import serve
+  from flask import request
+
+  logging.getLogger('waitress').setLevel(logging.ERROR)
+
+  if network:
+    host_address = '0.0.0.0'
+    ip_address = socket.gethostbyname(socket.gethostname())
+
+  printc.say("Server running on " + printc.colors.BLUE + "http://" + ip_address + ":" + str(port) + '/' + printc.colors.ENDC, end="", script_name=script_name)
+  if network:
+    print(" (network-accessible)")
+  else:
+    print(" (localhost only)")
+  printc.say("press 'q' to quit", script_name=script_name)
+
+  # Open the web page
+  process = Thread(target=open_browser).start()
+
+  result_explorer = ResultExplorer(
+    result_path=input
+  )
+
+  # Start the server
+  serve_thread = Thread(target=serve, args=(result_explorer.app.server,), kwargs={'host': host_address, 'port': port})
+  serve_thread.start()
+
+  old_settings = set_raw_mode()
+  try:
+    while True:
+      # Check if a key is pressed
+      if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        key = sys.stdin.read(1).lower()
+        if key == 'q':
+          close_server()
+  finally:
+    restore_mode(old_settings)
+
+######################################
+# Main
+######################################
+
+def main(args):
+  network = args.network
+  input = args.input
+  start_result_explorer(input, network)
+
 
 if __name__ == "__main__":
-    from waitress import serve
-    from flask import request
-
-    logging.getLogger('waitress').setLevel(logging.ERROR)
-
-    if network:
-        host_address = '0.0.0.0'
-        ip_address = socket.gethostbyname(socket.gethostname())
-
-    printc.say("Server running on " + printc.colors.BLUE + "http://" + ip_address + ":" + str(port) + '/' + printc.colors.ENDC, end="", script_name=script_name)
-    if network:
-        print(" (network-accessible)")
-    else:
-        print(" (localhost only)")
-    printc.say("press 'q' to quit", script_name=script_name)
-
-    # Open the web page
-    process = Thread(target=open_browser).start()
-
-    result_explorer = ResultExplorer(
-        result_path=args.input
-    )
-
-    # Start the server
-    serve_thread = Thread(target=serve, args=(result_explorer.app.server,), kwargs={'host': host_address, 'port': port})
-    serve_thread.start()
-
-    old_settings = set_raw_mode()
-    try:
-        while True:
-            # Check if a key is pressed
-            if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-                key = sys.stdin.read(1).lower()
-                if key == 'q':
-                    close_server()
-    finally:
-        restore_mode(old_settings)
+  args = parse_arguments()
+  main(args)
