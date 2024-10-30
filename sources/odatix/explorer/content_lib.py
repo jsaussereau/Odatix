a0d067910eccde08045a44c1088787805203e118
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-10-30 18:24:51 +0100

    Add a html content lib

---
+++ sources/odatix/explorer/content_lib.py
@@ -0,0 +1,36 @@
+# ********************************************************************** #
+#                                Odatix                                  #
+# ********************************************************************** #
+#
+# Copyright (C) 2022 Jonathan Saussereau
+#
+# This file is part of Odatix.
+# Odatix is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Odatix is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Odatix. If not, see <https://www.gnu.org/licenses/>.
+#
+
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import traceback
+
+def generate_error_div(e):
+  error_traceback = traceback.format_exc()
+
+  return html.Div(className="error", children=[
+    html.Div("Unexpected error: " + str(e)),
+    html.Details([
+        html.Summary("Error details"),
+        html.Pre(error_traceback) 
+    ])
+  ])
