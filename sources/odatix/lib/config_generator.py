94e39cec6d4dabd1cf26556c134797f3f8f8f199
Jonathan Saussereau <jsaussereau@protonmail.com>
2025-02-23 16:56:59 +0100

    Fix union (concatenation) not working as intended

--- sources/odatix/lib/config_generator.py
+++ sources/odatix/lib/config_generator.py
@@ -1,4 +1,3 @@
-
 # ********************************************************************** #
 #                                Odatix                                  #
 # ********************************************************************** #
@@ -28,7 +27,7 @@ import itertools
 
 import odatix.lib.printc as printc
 import odatix.lib.hard_settings as hard_settings
-from odatix.lib.get_from_dict import get_from_dict, Key, KeyNotInDictError, BadValueInDictError
+from odatix.lib.get_from_dict import get_from_dict, Key
 
 script_name = os.path.basename(__file__)
 
@@ -47,14 +46,48 @@ class ConfigGenerator:
     Initialize the configuration generator by loading the YAML settings.
     
     Args:
-        yaml_file (str): Path to the YAML configuration file.
+        path (str): Path to the directory containing the "_settings.yml" file.
+        silent (bool): If True, suppress warnings for missing keys.
+        debug (bool): If True, print debug information.
     """
     self.path = path
     self.yaml_file = os.path.join(self.path, hard_settings.param_settings_filename)
-    hide = not debug
-    settings = self._load_yaml()
-    generate_enabled, generate_defined = get_from_dict("generate_configurations", settings, self.yaml_file, default_value=False, silent=hide, script_name=script_name)
-    generate_settings, generate_settings_defined = get_from_dict("generate_configurations_settings", settings, self.yaml_file, silent=hide, script_name=script_name)
+    self.silent = silent
+    self.debug = debug
+
+    self.template = ""
+    self.name_template = ""
+    self.variables = {}
+    self.valid = False
+    self.enabled = False
+
+    self._load_yaml()
+
+  def _load_yaml(self):
+    """
+    Load YAML settings and validate the presence of necessary keys.
+    
+    Returns:
+        dict: Parsed YAML data.
+    """
+    if not os.path.isfile(self.yaml_file):
+      printc.error(f"YAML file '{self.yaml_file}' does not exist.")
+      sys.exit(-1)
+
+    try:
+      with open(self.yaml_file, "r") as f:
+        data = yaml.safe_load(f)
+        if data is None:
+          printc.error(f"YAML file \"{self.yaml_file}\" is empty!")
+          sys.exit(-1)
+    except yaml.YAMLError as e:
+      printc.error(f"Invalid YAML file \"{self.yaml_file}\": {e}")
+      sys.exit(-1)
+
+    # Check main keys
+    hide = not self.debug
+    generate_enabled, generate_defined = get_from_dict("generate_configurations", data, self.yaml_file, default_value=False, silent=hide, script_name=script_name)
+    generate_settings, generate_settings_defined = get_from_dict("generate_configurations_settings", data, self.yaml_file, silent=hide, script_name=script_name)
     
     if generate_settings_defined:
       self.template, template_defined = get_from_dict("template", generate_settings, self.yaml_file, parent="generate_configurations_settings", type=str, behavior=Key.MANTADORY, script_name=script_name)
@@ -64,37 +97,13 @@ class ConfigGenerator:
     else:
       self.valid = False
 
-    if not generate_defined and generate_settings_defined and not self.silent:
+    if not generate_defined and generate_settings_defined and not silent:
       printc.warning('"generate_configurations_settings" is defined while "generate_configurations" is not. Disabling configuration generation.', script_name)
-    if generate_defined and generate_enabled and not generate_settings_defined and not self.silent:
+    if generate_defined and generate_enabled and not generate_settings_defined and not silent:
       printc.error('Configuration generation is enabled while "generate_configurations_settings" is not defined.', script_name)
 
     self.enabled = generate_enabled
-    self.debug = debug
-    self.silent = silent
 
-  def _load_yaml(self):
-    """
-    Load the YAML configuration file.
-    
-    Returns:
-        dict: Parsed YAML data.
-    """
-    if not os.path.isfile(self.yaml_file):
-      printc.error(f"YAML file '{self.yaml_file}' does not exist.")
-      sys.exit(-1)
-
-    with open(self.yaml_file, "r") as f:
-      try:
-        data = yaml.safe_load(f)
-        if data is None:
-          printc.error(f"YAML file \"{self.yaml_file}\" is empty!")
-          sys.exit(-1)
-        return data
-      except yaml.YAMLError as e:
-        printc.error(f"Invalid YAML file \"{self.yaml_file}\": {e}")
-        sys.exit(-1)
-    
   def evaluate_expression(self, expr, values_map):
     """
     Evaluate a mathematical expression using the current values of variables.
@@ -121,66 +130,108 @@ class ConfigGenerator:
   def generate(self):
     """
     Generate all possible parameter combinations based on the configuration.
+    If a variable is 'union', it merges the union of its sources into a single dimension.
     
     Returns:
         dict: A dictionary where keys are generated names and values are formatted templates.
     """
-    values_dict = {}
-
     if not self.valid or not self.enabled:
-      return values_dict
+      return {}
 
-    # Generate all possible values for each variable except function and concatenate types
-    generated_values = {var: self.generate_values(var_config, var) for var, var_config in self.variables.items() if var_config["type"] not in ["function", "concatenate"]}
-    
-    if self.debug:
-      print(f"Generated values per variable: {generated_values}")
+    # Identify which variables are sources for 'union'
+    sources_used = set()
+    for variable, config in self.variables.items():
+      value_type, value_type_defined = get_from_dict("type", config, self.yaml_file, parent=variable, behavior=Key.MANTADORY, script_name=script_name)
+      if value_type == "union":
+        settings, _ = get_from_dict("settings", config, self.yaml_file, silent=True, script_name=script_name)
+        if settings:
+          sources, _ = get_from_dict("sources", settings, self.yaml_file, silent=True, script_name=script_name)
+          for source in sources:
+            sources_used.add(source)
 
-    # Create cartesian product of all variable values
-    variable_names = list(generated_values.keys())
-    all_combinations = list(itertools.product(*generated_values.values()))
-    if self.debug:
-      print(f"Total combinations to process: {len(all_combinations)}")
+    #  Create dimension_vars for variables that are NOT used as sources in 'union' AND are not type='union'/'function'
+    dimension_vars = {}
+    for variable, config in self.variables.items():
+      value_type, value_type_defined = get_from_dict("type", config, self.yaml_file, parent=variable, behavior=Key.MANTADORY, script_name=script_name)
+      if value_type in ("range", "list", "multiples", "power_of_two") and variable not in sources_used:
+        # This is a dimension variable
+        dimension_vars[variable] = self.generate_values_for_dim(variable, config)
 
-    # Generate the final dictionary
-    for combination in all_combinations:
-      value_map = dict(zip(variable_names, combination))
-
-      # Handle function type variables
-      for var, config in self.variables.items():
-        value_type, _ = get_from_dict("type", config, self.yaml_file, silent=True, default_value="", script_name=script_name)
-        if value_type == "function":
-          settings, settings_defined = get_from_dict("settings", config, self.yaml_file, behavior=Key.MANTADORY, script_name=script_name)
-          if settings_defined:
-            op, _ = get_from_dict("op", settings, self.yaml_file, behavior=Key.MANTADORY, script_name=script_name)
-            value_map[var] = self.evaluate_expression(op, value_map)
-
-      # Handle concatenation type
-      for var, config in self.variables.items():
-        value_type, value_type_defined = get_from_dict("type", config, self.yaml_file, silent=True, default_value="", script_name=script_name)
-        if value_type == "concatenate":
-          settings, settings_defined = get_from_dict("settings", config, self.yaml_file, behavior=Key.MANTADORY, script_name=script_name)
-          if settings_defined:
-            sources, _ = get_from_dict("sources", settings, self.yaml_file, behavior=Key.MANTADORY, script_name=script_name)
-            value_map[var] = "".join(str(value_map[source]) for source in sources)
-
-      # Format values
-      formatted_values = {var: self.format_value(value_map[var], self.variables[var].get("format", "{}")) for var in value_map}
-
-      # Replace in template and name
-      formatted_template = self.template
-      formatted_name = self.name_template
-
-      for var, value in formatted_values.items():
-        formatted_template = formatted_template.replace(f"${var}", str(value)).replace(f"${{{var}}}", str(value))
-        formatted_name = formatted_name.replace(f"${var}", str(value)).replace(f"${{{var}}}", str(value))
-
-      values_dict[formatted_name] = formatted_template
+    # Create dimension for each 'union' variable => union of its sources
+    for variable, config in self.variables.items():
+      value_type, value_type_defined = get_from_dict("type", config, self.yaml_file, parent=variable, behavior=Key.MANTADORY, script_name=script_name)
+      if value_type == "union":
+        settings, settings_defined = get_from_dict("settings", config, self.yaml_file, silent=True, script_name=script_name)
+        if settings_defined:
+          sources, _ = get_from_dict("sources", settings, self.yaml_file, silent=True, script_name=script_name)
+          union_set = set()
+          for source in sources:
+            # Generate or retrieve the values from the source
+            config = self.variables.get(source)
+            if config:
+              # If the source has not been put in dimension_vars, we generate now
+              vals = self.generate_values_for_dim(source, config)
+              for val in vals:
+                union_set.add(val)
+            else:
+              printc.warning(f"Unknown source '{source}' in variable '{variable}'", script_name)
+              continue
+          union_list = sorted(union_set)
+          dimension_vars[variable] = union_list
 
     if self.debug:
-      print(f"\nGenerated Parameters ({len(values_dict)} total):")
+      printc.note(f"dimension_vars after union: {dimension_vars}", script_name)
 
-    return values_dict
+    # Build the product of all dimension_vars
+    var_names = list(dimension_vars.keys())
+    combos = list(itertools.product(*(dimension_vars[k] for k in var_names)))
+    if self.debug:
+      printc.note(f"total dimension combos = {len(combos)} from {list(dimension_vars.keys())}", script_name)
+
+    final_configs = {}
+
+    # For each combination, build a value_map
+    for combo in combos:
+      value_map = dict(zip(var_names, combo))
+
+      # Evaluate 'function' variables => single pass
+      for fn, fc in self.variables.items():
+        if fc.get("type") == "function":
+          settings, ok = get_from_dict("settings", fc, self.yaml_file, silent=True, script_name=script_name)
+          if ok:
+            op, _ = get_from_dict("op", settings, self.yaml_file, silent=True, script_name=script_name)
+            value_map[fn] = self.evaluate_expression(op, value_map)
+
+      # Format all variables
+      formatted_values = {}
+      for k, v in value_map.items():
+        fmt = self.variables.get(k, {}).get("format", "{}")
+        formatted_values[k] = self.format_value(v, fmt)
+
+      # Replace placeholders in template / name
+      final_template = self.template
+      final_name = self.name_template
+      for k, v in formatted_values.items():
+        final_template = final_template.replace(f"${k}", str(v)).replace(f"${{{k}}}", str(v))
+        final_name = final_name.replace(f"${k}", str(v)).replace(f"${{{k}}}", str(v))
+
+      final_configs[final_name] = final_template
+
+    if self.debug:
+      printc.note(f"generated {len(final_configs)} configurations.", script_name)
+
+    return final_configs
+
+  def generate_values_for_dim(self, var_name, var_config):
+    """
+    Generate the list of values for a dimension variable (range/list/multiples/power_of_two).
+    If it's 'function' or 'union', we skip for dimension creation here.
+    """
+    value_type, value_type_defined = get_from_dict("type", var_config, self.yaml_file, behavior=Key.MANTADORY, script_name=script_name)
+    if value_type in ("function", "union"):
+      return []
+
+    return self.generate_values(var_config, var_name)
 
   def generate_values(self, config, name):
     """
@@ -287,6 +338,8 @@ class ConfigGenerator:
     Returns:
         str: Formatted value as a string.
     """
+    if format_string is None:
+      return value
     if isinstance(value, list):
       return "".join(str(v) for v in value)
 
@@ -294,4 +347,3 @@ class ConfigGenerator:
       return format_string % value
     except TypeError:
       return str(value)
-
