a3268b5e2120ec1cff51c77975928f8e9fa5ce49
Mathias Michelotti <mathias.michelotti@ecomp.ufsm.br>
2026-08-19 13:17:26 +0200

    Add 22nm technology support

---
+++ sources/odatix_eda_tools/genus/technology/globalfoundaries22nhsp/genus_setup.tcl
@@ -0,0 +1,94 @@
+#################################################################################
+# GLOBALFOUNDRIES 22FDX - GENUS SETUP
+#################################################################################
+
+#-----------------------------------------------------------------------------
+# TECHNOLOGY PATHS
+#-----------------------------------------------------------------------------
+
+set PDK_PATH \
+    /asic/pdk/globalfoundries/22FDX-PLUS/V1.0_3.4
+
+set LIB_ROOT \
+    /asic/ip/DesignWare_logic_libs/globalfoundaries22nhsp/32hd116/hdl/lvt/2.00a
+
+set LIB_PATH \
+    $LIB_ROOT/liberty/logic_synth_lvf
+
+set CELL_LEF_PATH \
+    $LIB_ROOT/lef/5.8
+
+# Example metal stack
+set TECH_LEF_PATH \
+    $PDK_PATH/PlaceRoute/Innovus/Techfiles/10M_2Mx_4Cx_2Bx_2Jx_LB
+
+
+#-----------------------------------------------------------------------------
+# SEARCH PATHS
+#-----------------------------------------------------------------------------
+
+set LIB_SEARCH_PATHS [list]
+
+lappend LIB_SEARCH_PATHS .
+lappend LIB_SEARCH_PATHS $LIB_PATH
+
+set_db init_lib_search_path $LIB_SEARCH_PATHS
+
+
+#-----------------------------------------------------------------------------
+# TIMING LIBRARIES
+#-----------------------------------------------------------------------------
+
+set LIBS [list]
+
+lappend LIBS \
+    gf22nspllogl32hdl116f_SSG_0P72V_0P00V_0P60V_M1P00V_125C.lib.gz
+
+
+#-----------------------------------------------------------------------------
+# READ TIMING LIBRARIES
+#-----------------------------------------------------------------------------
+
+#foreach lib $LIBS {
+#    read_libs $lib
+#}
+read_mmmc scripts/genus_mmmc.tcl
+
+#-----------------------------------------------------------------------------
+# PHYSICAL LIBRARIES
+#-----------------------------------------------------------------------------
+
+set LEFS [list]
+
+# Technology / routing stack
+lappend LEFS \
+    $TECH_LEF_PATH/22FDSOI_10M_2Mx_4Cx_2Bx_2Jx_LB_116cpp_tech.lef
+
+# Standard placement-site definitions
+lappend LEFS \
+    $PDK_PATH/PlaceRoute/Innovus/Techfiles/22fdsoi_standard_site.lef
+
+# Standard-cell physical library
+lappend LEFS \
+    $CELL_LEF_PATH/gf22nspllogl32hdl116f.lef
+
+
+#-----------------------------------------------------------------------------
+# READ PHYSICAL LIBRARIES
+#-----------------------------------------------------------------------------
+
+read_physical -lefs $LEFS
+
+
+#################################################################################
+# HDL / NETLIST OPTIONS
+#################################################################################
+
+# Bus naming
+#set_db hdl_bus_naming_style {%s_%d}
+
+# Preserve hierarchy
+#set_db preserve_hierarchy true
+
+# Internal buses
+#set_db hdl_track_filename_row_col true
