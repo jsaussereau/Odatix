785a0b6efb8aa28890f31ed5693daa3d99afb30e
Mathias Michelotti <mathias.michelotti@ecomp.ufsm.br>
2026-08-25 15:03:22 +0200

    Update synth_script to choose between logical and physical synthesis

--- sources/odatix_eda_tools/genus/tcl/synth_script.tcl
+++ sources/odatix_eda_tools/genus/tcl/synth_script.tcl
@@ -17,24 +17,18 @@ source scripts/settings.tcl
 # SELECT SYNTHESIS FLOW
 #################################################################################
 
-if {$synth_mode eq "logical"} {
-
-    puts "=========================================="
-    puts "Selected synthesis mode: LOGICAL"
-    puts "=========================================="
-
-    source scripts/synth_script_logical.tcl
-
-} elseif {$synth_mode eq "physical"} {
-
+if {$::synth_mode eq "physical"} {
     puts "=========================================="
     puts "Selected synthesis mode: PHYSICAL"
     puts "=========================================="
 
-    source scripts/synth_script_physical.tcl
+    source scripts/step_physical_synthesis.tcl
+} elseif {$::synth_mode eq "logical"} {
+    puts "=========================================="
+    puts "Selected synthesis mode: LOGICAL"
+    puts "=========================================="
 
+    source scripts/step_logical_synthesis.tcl
 } else {
-
-    error "Invalid synthesis mode '$synth_mode'. Use 'logical' or 'physical'."
-
+    error "Invalid synthesis mode '$::synth_mode'"
 }
