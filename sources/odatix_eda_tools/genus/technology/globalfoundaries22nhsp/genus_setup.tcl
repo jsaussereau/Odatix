15517b5b93d95392c877e280af3e4826a02871dd
Mathias Michelotti <mathias.michelotti@ecomp.ufsm.br>
2026-08-26 13:47:42 +0200

    Fix inconsistencies in Genus setup

--- sources/odatix_eda_tools/genus/technology/globalfoundaries22nhsp/genus_setup.tcl
+++ sources/odatix_eda_tools/genus/technology/globalfoundaries22nhsp/genus_setup.tcl
@@ -7,22 +7,13 @@
 #-----------------------------------------------------------------------------
 set ::LIB_TYPE GF_22nm
 
-set PDK_PATH \
-    /asic/pdk/globalfoundries/22FDX-PLUS/V1.0_3.4
+set PDK_PATH /asic/pdk/globalfoundries/22FDX-PLUS/V1.0_3.4
 
-set LIB_ROOT \
-    /asic/ip/DesignWare_logic_libs/globalfoundaries22nhsp/32hd116/hdl/lvt/2.00a
+set LIB_ROOT /asic/ip/DesignWare_logic_libs/globalfoundaries22nhsp/32hd116/hdl/lvt/2.00a
 
-set LIB_PATH \
-    $LIB_ROOT/liberty/logic_synth_lvf
+set LIB_PATH $LIB_ROOT/liberty/logic_synth_lvf
 
-set CELL_LEF_PATH \
-    $LIB_ROOT/lef/5.8
-
-# Example metal stack
-set TECH_LEF_PATH \
-    $PDK_PATH/PlaceRoute/Innovus/Techfiles/10M_2Mx_4Cx_2Bx_2Jx_LB
-
+set CELL_LEF_PATH $LIB_ROOT/lef/5.8
 
 #-----------------------------------------------------------------------------
 # SEARCH PATHS
@@ -40,11 +31,8 @@ set_db init_lib_search_path $LIB_SEARCH_PATHS
 # TIMING LIBRARIES
 #-----------------------------------------------------------------------------
 
-set LIBS [list]
-
-lappend LIBS \
-    gf22nspllogl32hdl116f_SSG_0P72V_0P00V_0P60V_M1P00V_125C.lib.gz
-
+#set LIBS [list]
+#lappend LIBS gf22nspllogl32hdl116f_SSG_0P72V_0P00V_0P60V_M1P00V_125C.lib.gz
 
 #-----------------------------------------------------------------------------
 # READ TIMING LIBRARIES
@@ -66,20 +54,18 @@ read_mmmc scripts/genus_mmmc.tcl
 #-----------------------------------------------------------------------------
 # PHYSICAL LIBRARIES
 #-----------------------------------------------------------------------------
+set TECH_LEF_PATH $PDK_PATH/PlaceRoute/Innovus/Techfiles/10M_2Mx_6Cx_2Ix_LB
 
 set LEFS [list]
 
 # Technology / routing stack
-lappend LEFS \
-    $TECH_LEF_PATH/22FDSOI_10M_2Mx_4Cx_2Bx_2Jx_LB_116cpp_tech.lef
+lappend LEFS $TECH_LEF_PATH/22FDSOI_10M_2Mx_6Cx_2Ix_LB_116cpp_tech.lef
 
 # Standard placement-site definitions
-lappend LEFS \
-    $PDK_PATH/PlaceRoute/Innovus/Techfiles/22fdsoi_standard_site.lef
+lappend LEFS $PDK_PATH/PlaceRoute/Innovus/Techfiles/22fdsoi_standard_site.lef
 
 # Standard-cell physical library
-lappend LEFS \
-    $CELL_LEF_PATH/gf22nspllogl32hdl116f.lef
+lappend LEFS $CELL_LEF_PATH/gf22nspllogl32hdl116f.lef
 
 
 #-----------------------------------------------------------------------------
