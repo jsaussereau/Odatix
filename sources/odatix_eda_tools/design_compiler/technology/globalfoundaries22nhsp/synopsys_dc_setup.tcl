168381cfcacdad08bc8292eb62bb8e724fb0db51
Mathias Michelotti <mathias.michelotti@ecomp.ufsm.br>
2026-09-04 10:21:04 +0200

    Add 22nm support for Design Compiler

---
+++ sources/odatix_eda_tools/design_compiler/technology/globalfoundaries22nhsp/synopsys_dc_setup.tcl
@@ -0,0 +1,44 @@
+
+#########
+### Define Work Library Location
+#########
+#define_design_lib WORK -path "./work"
+
+#########
+### retrieve AMS directory path and SYNOPSYS path from the environement variables
+#########
+
+if {[catch {
+    set AMSDIR [get_unix_variable AMS_H18_5V0]
+} errmsg]} {
+    set AMSDIR "/opt/ams/"
+}
+
+set SYNDIR [get_unix_variable SYNOPSYS]
+
+#########
+### set library paths
+#########
+set GF22DIR "/asic/ip/DesignWare_logic_libs/globalfoundaries22nhsp/32hd116/hdl/lvt/2.00a/liberty/logic_synth_lvf"
+set search_path	". $GF22DIR $SYNDIR/libraries/syn $SYNDIR/dw/sim_ver"
+set target_library "gf22nspllogl32hdl116f_SSG_0P72V_0P00V_0P60V_M1P00V_125C.db"
+
+# set synthetic_library dw_foundation.sldb 
+set link_library "* $target_library $synthetic_library"
+
+#########
+### set bus naming style for compliance between .sdf and post syn .vhdl
+#########
+set sdfout_no_edge  true
+#set verilogout_equation	false
+#set verilogout_no_tri	true 
+#set verilogout_single_bit  false
+
+set hdlout_internal_busses true     
+set vhdlout_single_bit user
+set vhdlout_preserve_hierarchical_types user
+set bus_inference_style "%s_%d_"  
+set bus_naming_style    "%s_%d"
+set write_name_nets_same_as_ports true
+
+#puts "USE: set_fix_multiple_port_nets -all [all_designs]"
